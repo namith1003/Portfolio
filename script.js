66be0136af6485bfa4c8c6bbd60d680e624c482c
Namith <namithspider@gmail.com>
2024-06-27 02:01:36 +0800

    adjusted the timeline code to be moredynamic and animated.

--- script.js
+++ script.js
@@ -119,4 +119,19 @@ circleTL.to(circle, {
 });
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+      });
+    }, { threshold: 0.5 });
+  
+    document.querySelectorAll('.timeline-content').forEach((item) => {
+      observer.observe(item);
+    });
+  });
 
+
+  
